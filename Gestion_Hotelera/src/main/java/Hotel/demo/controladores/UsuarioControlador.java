10a83354a70a54481225be1f604661255983e8cc
Christian Bergometti <chbeavant@gmail.com>
2022-05-31 23:31:01 -0300

    Eliminación de plantillas prediseñadas por NetBeans

--- Gestion_Hotelera/src/main/java/Hotel/demo/controladores/UsuarioControlador.java
+++ Gestion_Hotelera/src/main/java/Hotel/demo/controladores/UsuarioControlador.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hotel.demo.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author Diego
- */
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioControlador {
